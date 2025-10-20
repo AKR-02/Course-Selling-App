aa88e691d966f477e5c6f09fec93d6fb4d64a6eb
AKR-02 <rautanshuman388@gmail.com>
2025-10-20 15:39:30 +0530

    added frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css'
+import Home from './pages/Home'
+import Courses from './pages/Courses'
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/courses" element={<Courses />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
